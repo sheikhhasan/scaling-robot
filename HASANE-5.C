f8af3669f96b0b389d2a92c8f35bab38a82d2e81
sheikhhasan <shasanbs@gmail.com>
2016-09-21 08:10:32 +0500

    Add files via upload

---
+++ HASANE-5.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+	void main()
+ {
+  int x,y;
+  clrscr();
+  printf("\n Enter Value For x=\t");
+  scanf("%d",&x);
+  printf("\n Enter Value For y=\t");
+  scanf("%d",&y);
+  if(x==y)
+ {
+  printf("\n NUumbers Are Equal");
+ }
+  else
+ {
+  printf("\n Numbers Are Unequal");
+ }
+  getch();
+ }
